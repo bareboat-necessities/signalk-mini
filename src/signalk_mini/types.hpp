c01573dab26fbc2de377ae12826fc2c2ab6fd674
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 18:49:48 -0400

    add signalk mini types header

---
+++ src/signalk_mini/types.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace signalk_mini {
+
+using SourceId = uint16_t;
+
+enum class ModelField : uint16_t {
+    None = 0,
+    NavigationGpsFixLatDeg,
+    NavigationGpsFixLonDeg,
+    NavigationGpsSpeedKn,
+    NavigationGpsTrackDeg,
+    ImuHeadingDeg,
+    WindApparentDirectionDeg,
+    WindApparentSpeedKn,
+    WaterDepthM,
+};
+
+} // namespace signalk_mini
