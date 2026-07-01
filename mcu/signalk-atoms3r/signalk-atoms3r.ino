7156497f114057621f5d1b72c3ca89b3c27d2a36
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 18:55:40 -0400

    wire atoms3r sketch to mini server

--- mcu/signalk-atoms3r/signalk-atoms3r.ino
+++ mcu/signalk-atoms3r/signalk-atoms3r.ino
@@ -1,10 +1,23 @@
-#include <signalk_mini.hpp>
+#define ASYNC_EVENT_LOOP_ENABLE_ARDUINO_WIFI_TCP
+#include <Arduino.h>
+#include <signalk_mini/server.hpp>
 
-signalk_mini::SignalKMiniApp<float> app;
+signalk_mini::SignalKMiniConfig config;
+signalk_mini::MiniSignalKServer<float> app(config);
+bool setup_failed = false;
 
 void setup() {
   Serial.begin(115200);
+  if (!app.begin()) {
+    Serial.println("signalk-mini setup failed");
+    setup_failed = true;
+  }
 }
 
 void loop() {
+  if (setup_failed) {
+    delay(1000);
+    return;
+  }
+  app.tick();
 }
