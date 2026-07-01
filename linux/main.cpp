6d6545045f92a3621420c439bd3e0db25738fd9c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 19:24:14 -0400

    start app from linux main

--- linux/main.cpp
+++ linux/main.cpp
@@ -1,15 +1,9 @@
-#include <iostream>
-#include <string>
 #include <signalk_mini.hpp>
 
 int main() {
-    signalk_mini::SignalKMiniApp<float> app;
-    std::string line;
-    uint64_t now_us = 0;
-    while (std::getline(std::cin, line)) {
-        now_us += 100000;
-        app.nmea0183().feed_line(line.c_str(), 1, now_us);
-    }
-    std::cout << "changes=" << app.store().sequence() << "\n";
+    signalk_mini::SignalKMiniConfig cfg;
+    signalk_mini::SignalKMiniApp<float> app(cfg);
+    if (!app.begin()) return 1;
+    app.run_forever();
     return 0;
 }
