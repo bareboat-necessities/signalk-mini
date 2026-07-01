ae1ba62281e54bcb68b390a1121fbe1b98804a61
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 18:54:37 -0400

    update umbrella header includes

--- src/signalk_mini.hpp
+++ src/signalk_mini.hpp
@@ -1,3 +1,10 @@
 #pragma once
 
+#include "signalk_mini/config.hpp"
 #include "signalk_mini/types.hpp"
+#include "signalk_mini/units.hpp"
+#include "signalk_mini/model_store.hpp"
+#include "signalk_mini/nmea0183_input.hpp"
+#include "signalk_mini/signalk_mapper.hpp"
+#include "signalk_mini/json/signalk_delta_writer.hpp"
+#include "signalk_mini/app.hpp"
