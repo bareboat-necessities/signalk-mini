039bf17bae41ebf0cf71101b5061b0541bec12a5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 19:22:40 -0400

    add nmea tcp client config

--- src/signalk_mini/config.hpp
+++ src/signalk_mini/config.hpp
@@ -18,6 +18,12 @@ struct SignalKMiniConfig {
     ServerIdentityConfig identity;
     bool enable_signalk_tcp = true;
     bool enable_nmea0183_tcp = true;
+    bool enable_nmea0183_tcp_client = true;
+    bool enable_nmea0183_tcp_server = false;
+    const char* nmea0183_tcp_client_host = "127.0.0.1";
+    uint16_t nmea0183_tcp_client_port = 10110;
+    const char* nmea0183_tcp_server_host = "0.0.0.0";
+    uint16_t nmea0183_tcp_server_port = 20224;
     uint32_t publish_interval_us = 10000;
     uint16_t max_changes_per_tick = 32;
     uint16_t json_buffer_size = 512;
