48a3d4315f47776792e1157b8e9760089737764c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 20:15:33 -0400

    rename runtime clients sources to connections

--- src/signalk_mini/server.hpp
+++ src/signalk_mini/server.hpp
@@ -14,20 +14,20 @@
 
 namespace signalk_mini {
 
-template<typename Real, size_t MaxSignalKClients = 8, size_t MaxNmeaSources = 4>
+template<typename Real, size_t MaxSignalKConnections = 8, size_t MaxNmeaSourceConnections = 4>
 class MiniSignalKServer {
 public:
-    explicit MiniSignalKServer(const SignalKMiniConfig& cfg = SignalKMiniConfig{})
-        : cfg_(cfg), sk_server_(loop_.scheduler()), nmea_server_(loop_.scheduler()),
-          nmea_client_(loop_.scheduler()), publisher_(store_, cfg_), sk_(*this),
-          nmea_server_cb_(*this), nmea_client_cb_(*this),
-          sk_lines_(sk_, sk_options()), nmea_lines_(nmea_server_cb_) {}
+    explicit MiniSignalKServer(const SignalKMiniConfig& config = SignalKMiniConfig{})
+        : config_(config), signalk_tcp_server_(loop_.scheduler()), nmea_tcp_server_(loop_.scheduler()),
+          nmea_tcp_client_(loop_.scheduler()), publisher_(store_, config_), signalk_connections_(*this),
+          nmea_server_connections_(*this), nmea_client_connection_(*this),
+          signalk_line_handler_(signalk_connections_, signalk_options()), nmea_line_handler_(nmea_server_connections_) {}
 
     bool begin() {
         if (!loop_.valid()) return false;
-        if (!listen(sk_server_, sk_lines_, cfg_.signalk.host, cfg_.signalk.port)) return false;
+        if (!listen(signalk_tcp_server_, signalk_line_handler_, config_.signalk.host, config_.signalk.port)) return false;
         start_configured_connectors();
-        timer_ = loop_.on_repeat_us(cfg_.publisher.interval_us, [this]() { publish(); });
+        timer_ = loop_.on_repeat_us(config_.publisher.interval_us, [this]() { publish(); });
         return static_cast<bool>(timer_);
     }
 
@@ -37,39 +37,42 @@ public:
     Nmea0183Input<Real>& nmea0183() { return nmea_; }
 
 private:
-    static async_event_loop::TcpLineServerOptions sk_options() {
-        async_event_loop::TcpLineServerOptions o;
+    static async_event_loop::TcpLineServerOptions signalk_options() {
+        async_event_loop::TcpLineServerOptions options;
 #if defined(ARDUINO)
-        o.backpressure = async_event_loop::TcpBackpressureOptions::embedded_default();
+        options.backpressure = async_event_loop::TcpBackpressureOptions::embedded_default();
 #else
-        o.backpressure = async_event_loop::TcpBackpressureOptions::server_default();
+        options.backpressure = async_event_loop::TcpBackpressureOptions::server_default();
 #endif
-        return o;
+        return options;
     }
 
-    bool listen(async_event_loop::NativeTcpServer& s, async_event_loop::ITcpServerHandler& h, const char* host, uint16_t port) {
-        async_event_loop::TcpListenOptions o;
-        o.host = host;
-        o.port = port;
-        o.reuse_address = true;
-        return s.listen(o, h);
+    bool listen(async_event_loop::NativeTcpServer& server,
+                async_event_loop::ITcpServerHandler& handler,
+                const char* host,
+                uint16_t port) {
+        async_event_loop::TcpListenOptions options;
+        options.host = host;
+        options.port = port;
+        options.reuse_address = true;
+        return server.listen(options, handler);
     }
 
     void start_configured_connectors() {
-        const size_t count = cfg_.connector_count <= max_connector_configs ? cfg_.connector_count : max_connector_configs;
+        const size_t count = config_.connector_count <= max_connector_configs ? config_.connector_count : max_connector_configs;
         for (size_t i = 0; i < count; ++i) {
-            const ConnectorConfig& c = cfg_.connectors[i];
-            if (!c.enabled) continue;
-            if (c.protocol == ConnectorProtocol::Nmea0183 && c.transport == ConnectorTransport::TcpClient && !nmea_client_started_) {
-                nmea_client_flags_ = ConnectionFlags{c.allow_rx, c.allow_tx};
-                async_event_loop::TcpConnectOptions o;
-                o.host = c.host;
-                o.port = c.port;
-                nmea_client_.connect(o, nmea_client_cb_);
-                nmea_client_started_ = true;
-            } else if (c.protocol == ConnectorProtocol::Nmea0183 && c.transport == ConnectorTransport::TcpServer && !nmea_server_started_) {
-                nmea_server_flags_ = ConnectionFlags{c.allow_rx, c.allow_tx};
-                nmea_server_started_ = listen(nmea_server_, nmea_lines_, c.host, c.port);
+            const ConnectorConfig& connector = config_.connectors[i];
+            if (!connector.enabled) continue;
+            if (connector.protocol == ConnectorProtocol::Nmea0183 && connector.transport == ConnectorTransport::TcpClient && !nmea_tcp_client_connector_started_) {
+                nmea_tcp_client_connection_flags_ = ConnectionFlags{connector.allow_rx, connector.allow_tx};
+                async_event_loop::TcpConnectOptions options;
+                options.host = connector.host;
+                options.port = connector.port;
+                nmea_tcp_client_.connect(options, nmea_client_connection_);
+                nmea_tcp_client_connector_started_ = true;
+            } else if (connector.protocol == ConnectorProtocol::Nmea0183 && connector.transport == ConnectorTransport::TcpServer && !nmea_tcp_server_connector_started_) {
+                nmea_tcp_server_connection_flags_ = ConnectionFlags{connector.allow_rx, connector.allow_tx};
+                nmea_tcp_server_connector_started_ = listen(nmea_tcp_server_, nmea_line_handler_, connector.host, connector.port);
             }
         }
     }
@@ -78,72 +81,75 @@ private:
         if (nmea_.feed_line(text, source_id, loop_.clock().micros())) publish();
     }
 
-    void publish() { publisher_.publish_pending(sk_.clients); }
+    void publish() { publisher_.publish_pending(signalk_connections_.connections); }
 
-    struct SkHandler : async_event_loop::ITcpLineServerHandler {
-        explicit SkHandler(MiniSignalKServer& o) : owner(o) {}
+    struct SignalKConnectionHandler : async_event_loop::ITcpLineServerHandler {
+        explicit SignalKConnectionHandler(MiniSignalKServer& owner_ref) : owner(owner_ref) {}
         MiniSignalKServer& owner;
-        ConnectionRegistry<MaxSignalKClients> clients;
-        void on_accept(async_event_loop::ITcpConnection& c, const async_event_loop::TcpPeerInfo&) override {
-            ConnectionFlags flags{owner.cfg_.signalk.allow_rx, owner.cfg_.signalk.allow_tx};
-            if (!clients.add(c, flags)) c.close();
+        ConnectionRegistry<MaxSignalKConnections> connections;
+        void on_accept(async_event_loop::ITcpConnection& connection, const async_event_loop::TcpPeerInfo&) override {
+            ConnectionFlags flags{owner.config_.signalk.allow_rx, owner.config_.signalk.allow_tx};
+            if (!connections.add(connection, flags)) connection.close();
         }
-        void on_line(async_event_loop::ITcpConnection& c, async_event_loop::LineView) override {
-            if (!clients.allow_rx(c)) return;
+        void on_line(async_event_loop::ITcpConnection& connection, async_event_loop::LineView) override {
+            if (!connections.allow_rx(connection)) return;
         }
-        void on_backpressure(async_event_loop::ITcpConnection& c, const async_event_loop::TcpBackpressureInfo&) override { clients.remove(c); c.close(); }
-        void on_close(async_event_loop::ITcpConnection& c) override { clients.remove(c); }
-        void on_error(async_event_loop::ITcpConnection& c, int) override { clients.remove(c); }
-        void on_too_many_connections(async_event_loop::ITcpConnection& c) override { c.close(); }
+        void on_backpressure(async_event_loop::ITcpConnection& connection, const async_event_loop::TcpBackpressureInfo&) override {
+            connections.remove(connection);
+            connection.close();
+        }
+        void on_close(async_event_loop::ITcpConnection& connection) override { connections.remove(connection); }
+        void on_error(async_event_loop::ITcpConnection& connection, int) override { connections.remove(connection); }
+        void on_too_many_connections(async_event_loop::ITcpConnection& connection) override { connection.close(); }
     };
 
-    struct NmeaServerHandler : async_event_loop::ITcpLineServerHandler {
-        explicit NmeaServerHandler(MiniSignalKServer& o) : owner(o) {}
+    struct NmeaTcpServerConnectionHandler : async_event_loop::ITcpLineServerHandler {
+        explicit NmeaTcpServerConnectionHandler(MiniSignalKServer& owner_ref) : owner(owner_ref) {}
         MiniSignalKServer& owner;
-        ConnectionRegistry<MaxNmeaSources> sources;
-        void on_accept(async_event_loop::ITcpConnection& c, const async_event_loop::TcpPeerInfo&) override {
-            if (!sources.add(c, owner.nmea_server_flags_)) c.close();
+        ConnectionRegistry<MaxNmeaSourceConnections> connections;
+        void on_accept(async_event_loop::ITcpConnection& connection, const async_event_loop::TcpPeerInfo&) override {
+            if (!connections.add(connection, owner.nmea_tcp_server_connection_flags_)) connection.close();
         }
-        void on_line(async_event_loop::ITcpConnection& c, async_event_loop::LineView line) override {
-            if (!sources.allow_rx(c)) return;
+        void on_line(async_event_loop::ITcpConnection& connection, async_event_loop::LineView line) override {
+            if (!connections.allow_rx(connection)) return;
             char text[160];
             async_event_loop::line_to_cstr(line, text);
             owner.handle_nmea_line(text, SourceId(1));
         }
-        void on_close(async_event_loop::ITcpConnection& c) override { sources.remove(c); }
-        void on_error(async_event_loop::ITcpConnection& c, int) override { sources.remove(c); }
-        void on_too_many_connections(async_event_loop::ITcpConnection& c) override { c.close(); }
+        void on_close(async_event_loop::ITcpConnection& connection) override { connections.remove(connection); }
+        void on_error(async_event_loop::ITcpConnection& connection, int) override { connections.remove(connection); }
+        void on_too_many_connections(async_event_loop::ITcpConnection& connection) override { connection.close(); }
     };
 
-    struct NmeaClientHandler : async_event_loop::ITcpClientHandler {
-        explicit NmeaClientHandler(MiniSignalKServer& o) : owner(o) {}
+    struct NmeaTcpClientConnectionHandler : async_event_loop::ITcpClientHandler {
+        explicit NmeaTcpClientConnectionHandler(MiniSignalKServer& owner_ref) : owner(owner_ref) {}
         MiniSignalKServer& owner;
-        void on_data(async_event_loop::ITcpConnection& c) override {
-            if (!owner.nmea_client_flags_.allow_rx) return;
+        void on_data(async_event_loop::ITcpConnection& connection) override {
+            if (!owner.nmea_tcp_client_connection_flags_.allow_rx) return;
             char text[160];
-            while (c.read_line(text, sizeof(text))) owner.handle_nmea_line(text, SourceId(2));
+            while (connection.read_line(text, sizeof(text))) owner.handle_nmea_line(text, SourceId(2));
         }
         void on_close(async_event_loop::ITcpConnection&) override {}
         void on_error(int) override {}
     };
 
-    SignalKMiniConfig cfg_;
+    SignalKMiniConfig config_;
     async_event_loop::EventLoop<> loop_;
-    async_event_loop::NativeTcpServer sk_server_;
-    async_event_loop::NativeTcpServer nmea_server_;
-    async_event_loop::NativeTcpClient nmea_client_;
+    async_event_loop::NativeTcpServer signalk_tcp_server_;
+    async_event_loop::NativeTcpServer nmea_tcp_server_;
+    async_event_loop::NativeTcpClient nmea_tcp_client_;
     ModelStore<Real> store_{};
     Nmea0183Input<Real> nmea_{store_};
     SignalKPublisher<Real> publisher_;
-    SkHandler sk_;
-    NmeaServerHandler nmea_server_cb_;
-    NmeaClientHandler nmea_client_cb_;
-    async_event_loop::TcpLineServerHandler<256, MaxSignalKClients> sk_lines_;
-    async_event_loop::TcpLineServerHandler<192, MaxNmeaSources> nmea_lines_;
-    ConnectionFlags nmea_server_flags_{true, false};
-    ConnectionFlags nmea_client_flags_{true, false};
-    bool nmea_server_started_ = false;
-    bool nmea_client_started_ = false;
+    SignalKConnectionHandler signalk_connections_;
+    NmeaTcpServerConnectionHandler nmea_server_connections_;
+    NmeaTcpClientConnectionHandler nmea_client_connection_;
+    async_event_loop::TcpLineServerHandler<256, MaxSignalKConnections> signalk_line_handler_;
+    async_event_loop::TcpLineServerHandler<192, MaxNmeaSourceConnections> nmea_line_handler_;
+    ConnectionFlags nmea_tcp_server_connection_flags_{true, false};
+    ConnectionFlags nmea_tcp_client_connection_flags_{true, false};
+    bool nmea_tcp_server_connector_started_ = false;
+    bool nmea_tcp_client_connector_started_ = false;
     async_event_loop::EventHandle timer_{};
 };
 
