34ba498dfa3abcf23a33038236995fa6c878e3a3
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 22:21:18 -0400

    separate connector protocol and transport config

--- src/signalk_mini/config.hpp
+++ src/signalk_mini/config.hpp
@@ -45,9 +45,60 @@ enum class ConnectorTransport : uint8_t {
     AnalogPin,
 };
 
+struct ConnectorAccessConfig {
+    bool allow_rx = true;
+    bool allow_tx = false;
+};
+
 struct Nmea0183ProtocolConfig {
     bool validate_checksum = false;
     bool validate_checksum_configured = false;
+};
+
+struct ConnectorProtocolConfig {
+    ConnectorProtocol kind = ConnectorProtocol::None;
+    Nmea0183ProtocolConfig nmea0183;
+};
+
+struct TcpClientTransportConfig {
+    const char* host = "127.0.0.1";
+    uint16_t port = 0;
+};
+
+struct TcpServerTransportConfig {
+    const char* host = "127.0.0.1";
+    uint16_t port = 0;
+};
+
+struct SerialTransportConfig {
+    const char* device = nullptr;
+    uint32_t baud = 4800;
+};
+
+struct UdpTransportConfig {
+    const char* host = "127.0.0.1";
+    uint16_t port = 0;
+    uint16_t local_port = 0;
+};
+
+struct I2cTransportConfig {
+    uint8_t bus = 0;
+    uint8_t address = 0;
+};
+
+struct PinTransportConfig {
+    uint8_t pin = 0;
+};
+
+struct ConnectorTransportConfig {
+    ConnectorTransport kind = ConnectorTransport::None;
+    TcpClientTransportConfig tcp_client;
+    TcpServerTransportConfig tcp_server;
+    SerialTransportConfig serial;
+    UdpTransportConfig udp;
+    I2cTransportConfig i2c;
+    PinTransportConfig digital_pin;
+    PinTransportConfig analog_pin;
 };
 
 inline bool default_nmea0183_validate_checksum(ConnectorTransport transport) {
@@ -60,23 +111,29 @@ inline bool effective_nmea0183_validate_checksum(const Nmea0183ProtocolConfig& p
 
 struct ConnectorConfig {
     bool enabled = false;
-    ConnectorProtocol protocol = ConnectorProtocol::None;
-    ConnectorTransport transport = ConnectorTransport::None;
     const char* label = nullptr;
-    const char* host = "127.0.0.1";
-    uint16_t port = 0;
-    const char* device = nullptr;
-    uint32_t baud = 4800;
-    uint8_t pin = 0;
-    uint8_t i2c_bus = 0;
-    uint8_t i2c_address = 0;
-    bool allow_rx = true;
-    bool allow_tx = false;
-    Nmea0183ProtocolConfig nmea0183;
+    ConnectorAccessConfig access;
+    ConnectorProtocolConfig protocol;
+    ConnectorTransportConfig transport;
 };
 
+inline ConnectorConfig make_default_nmea0183_tcp_client_connector() {
+    ConnectorConfig connector;
+    connector.enabled = true;
+    connector.label = "nmea0183-tcp-client";
+    connector.access.allow_rx = true;
+    connector.access.allow_tx = false;
+    connector.protocol.kind = ConnectorProtocol::Nmea0183;
+    connector.protocol.nmea0183.validate_checksum = false;
+    connector.protocol.nmea0183.validate_checksum_configured = true;
+    connector.transport.kind = ConnectorTransport::TcpClient;
+    connector.transport.tcp_client.host = "127.0.0.1";
+    connector.transport.tcp_client.port = 10110;
+    return connector;
+}
+
 inline const ConnectorConfig default_connector_configs[] = {
-    {true, ConnectorProtocol::Nmea0183, ConnectorTransport::TcpClient, "nmea0183-tcp-client", "127.0.0.1", 10110, nullptr, 4800, 0, 0, 0, true, false, {false, true}}
+    make_default_nmea0183_tcp_client_connector()
 };
 
 struct SignalKMiniConfig {
