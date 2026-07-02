98f6b8ec6316c5ec75ace1051d034d3bfcce30d5
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-02 17:11:57 -0400

    keep only multipart state in NMEA message state

--- modules/nmea0183/src/nmea0183_connector/nmea_message_state.hpp
+++ modules/nmea0183/src/nmea0183_connector/nmea_message_state.hpp
@@ -48,50 +48,7 @@ struct NmeaMultipartMessageRecord {
     uint64_t last_update_us = 0;
 };
 
-struct NmeaCurrentLayerMessageRecord {
-    NmeaMessageSource source;
-    NmeaMessageStampedInt layer_number;
-    float current_direction_deg = 0.0f;
-    char direction_reference = 0;
-    float current_speed_kn = 0.0f;
-    float layer_depth_m = 0.0f;
-    bool has_direction = false;
-    bool has_speed = false;
-    bool has_depth = false;
-    uint64_t last_update_us = 0;
-};
-
-struct NmeaVariablePointMessageRecord {
-    NmeaMessageSource source;
-    float time_to_point_s = 0.0f;
-    float distance_to_point_nmi = 0.0f;
-    char point_id[16] = {0};
-    bool has_time = false;
-    bool has_distance = false;
-    uint64_t last_update_us = 0;
-};
-
 struct NmeaMessageState {
-    NmeaRawMessageRecord alarm_acknowledgement;
-    NmeaRawMessageRecord automatic_device_status;
-    NmeaRawMessageRecord acknowledge_detail_alarm;
-    NmeaRawMessageRecord set_detail_alarm;
-    NmeaRawMessageRecord autopilot_system;
-    NmeaRawMessageRecord bearing_distance_dead_reckoning;
-    NmeaRawMessageRecord encryption_key_command;
-    NmeaRawMessageRecord operational_period_command;
-    NmeaCurrentLayerMessageRecord current_layer;
-    NmeaRawMessageRecord device_capability_report;
-    NmeaRawMessageRecord display_dimming_control;
-    NmeaRawMessageRecord door_status;
-    NmeaRawMessageRecord engine_telegraph;
-    NmeaRawMessageRecord event_message;
-    NmeaRawMessageRecord fire_detection;
-    NmeaRawMessageRecord waypoint_distance_rhumb;
-    NmeaRawMessageRecord waypoint_distance_great_circle;
-    NmeaVariablePointMessageRecord variable_point;
-
-    NmeaRawMessageRecord text_sentence;
     NmeaMultipartMessageRecord text_message;
     NmeaMultipartMessageRecord ais_message;
     NmeaMultipartMessageRecord navtex_message;
