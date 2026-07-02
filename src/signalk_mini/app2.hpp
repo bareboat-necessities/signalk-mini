31fce7594f6e6255c68b879ca966bdf20a97b5c7
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 21:27:09 -0400

    add app facade

---
+++ src/signalk_mini/app2.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "server_impl.hpp"
+
+namespace signalk_mini {
+
+template<typename Real = float>
+class SignalKMiniApp {
+public:
+    explicit SignalKMiniApp(const SignalKMiniConfig& config = SignalKMiniConfig{}) : server_(config) {}
+    bool begin() { return server_.begin(); }
+    void tick() { server_.tick(); }
+    void run_forever() { server_.run_forever(); }
+    ModelStore<Real>& store() { return server_.store(); }
+    Nmea0183Input<Real>& nmea0183() { return server_.nmea0183(); }
+private:
+    MiniSignalKServer<Real> server_;
+};
+
+} // namespace signalk_mini
