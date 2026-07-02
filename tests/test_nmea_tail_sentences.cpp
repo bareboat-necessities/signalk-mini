7c814d0cee2df3af603d9912a7cc7beef3994aab
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-02 15:46:24 -0400

    read protocol tail sentence state from NMEA connector

--- tests/test_nmea_tail_sentences.cpp
+++ tests/test_nmea_tail_sentences.cpp
@@ -85,84 +85,84 @@ int main() {
     NEAR(app.store().model().water.trawl_depth_below_surface_m.value, 56.7f, 0.001f);
 
     feed(app, "ALACK,42", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.alarm_acknowledgement.sentence_id, "ACK") == 0);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.alarm_acknowledgement.field[0], "42") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().alarm_acknowledgement.sentence_id, "ACK") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().alarm_acknowledgement.field[0], "42") == 0);
 
     feed(app, "AIADS,DEV1,A,OK", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.automatic_device_status.field[0], "DEV1") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().automatic_device_status.field[0], "DEV1") == 0);
 
     feed(app, "ALAKD,ALM1,A", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.acknowledge_detail_alarm.field[0], "ALM1") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().acknowledge_detail_alarm.field[0], "ALM1") == 0);
 
     feed(app, "ALALA,ALM2,SET", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.set_detail_alarm.field[1], "SET") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().set_detail_alarm.field[1], "SET") == 0);
 
     feed(app, "APASD,MODE1,DATA", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.autopilot_system.field[0], "MODE1") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().autopilot_system.field[0], "MODE1") == 0);
 
     feed(app, "GPBEC,112233,4917.24,N,12309.57,W,054.7,T,034.4,M,10.5,N,WPDR", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.bearing_distance_dead_reckoning.sentence_id, "BEC") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().bearing_distance_dead_reckoning.sentence_id, "BEC") == 0);
 
     feed(app, "CECEK,KEY1,ACTIVE", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.encryption_key_command.field[0], "KEY1") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().encryption_key_command.field[0], "KEY1") == 0);
 
     feed(app, "ECCOP,PERIOD,10", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.operational_period_command.field[1], "10") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().operational_period_command.field[1], "10") == 0);
 
     feed(app, "VCCUR,1,123.4,T,2.5,N,10.0,M", now_us);
-    REQUIRE(app.store().model().nmea_extensions.current_layer.layer_number.value == 1);
-    NEAR(app.store().model().nmea_extensions.current_layer.current_direction_deg.value, 123.4f, 0.001f);
-    NEAR(app.store().model().nmea_extensions.current_layer.current_speed_kn.value, 2.5f, 0.001f);
-    NEAR(app.store().model().nmea_extensions.current_layer.layer_depth_m.value, 10.0f, 0.001f);
+    REQUIRE(app.nmea0183().state().current_layer.layer_number.value == 1);
+    NEAR(app.nmea0183().state().current_layer.current_direction_deg, 123.4f, 0.001f);
+    NEAR(app.nmea0183().state().current_layer.current_speed_kn, 2.5f, 0.001f);
+    NEAR(app.nmea0183().state().current_layer.layer_depth_m, 10.0f, 0.001f);
 
     feed(app, "ECDCR,CAP,VALUE", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.device_capability_report.field[0], "CAP") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().device_capability_report.field[0], "CAP") == 0);
 
     feed(app, "INDDC,50,A", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.display_dimming_control.field[0], "50") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().display_dimming_control.field[0], "50") == 0);
 
     feed(app, "FDDOR,DOOR1,OPEN", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.door_status.field[1], "OPEN") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().door_status.field[1], "OPEN") == 0);
 
     feed(app, "CDDSC,12,3380400790,12,06,00,1423108312,2019,,,S,E", now_us);
-    REQUIRE(app.store().model().nmea_extensions.dsc.message.format_specifier.value == 12);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.dsc.message.sender_mmsi, "3380400790") == 0);
-    REQUIRE(app.store().model().nmea_extensions.dsc.message.category.value == 12);
-    REQUIRE(app.store().model().nmea_extensions.dsc.message.nature_or_first_telecommand.value == 6);
-    NEAR(app.store().model().nmea_extensions.dsc.message.latitude_deg.value, 42.516666f, 0.001f);
-    NEAR(app.store().model().nmea_extensions.dsc.message.longitude_deg.value, -83.2f, 0.001f);
-    NEAR(app.store().model().nmea_extensions.dsc.message.utc_time_s.value, 73140.0f, 0.001f);
-    REQUIRE(app.store().model().nmea_extensions.dsc.message.end_of_sequence == 'S');
-    REQUIRE(app.store().model().nmea_extensions.dsc.message.expansion_flag == 'E');
+    REQUIRE(app.nmea0183().state().dsc.message.format_specifier.value == 12);
+    REQUIRE(std::strcmp(app.nmea0183().state().dsc.message.sender_mmsi, "3380400790") == 0);
+    REQUIRE(app.nmea0183().state().dsc.message.category.value == 12);
+    REQUIRE(app.nmea0183().state().dsc.message.nature_or_first_telecommand.value == 6);
+    NEAR(app.nmea0183().state().dsc.message.latitude_deg, 42.516666f, 0.001f);
+    NEAR(app.nmea0183().state().dsc.message.longitude_deg, -83.2f, 0.001f);
+    NEAR(app.nmea0183().state().dsc.message.utc_time_s, 73140.0f, 0.001f);
+    REQUIRE(app.nmea0183().state().dsc.message.end_of_sequence == 'S');
+    REQUIRE(app.nmea0183().state().dsc.message.expansion_flag == 'E');
 
     feed(app, "CDDSE,1,1,A,3380400790,00,45894494", now_us);
-    REQUIRE(app.store().model().nmea_extensions.dsc.expansion.total_messages.value == 1);
-    REQUIRE(app.store().model().nmea_extensions.dsc.expansion.message_number.value == 1);
-    REQUIRE(app.store().model().nmea_extensions.dsc.expansion.query_flag == 'A');
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.dsc.expansion.sender_mmsi, "3380400790") == 0);
-    REQUIRE(app.store().model().nmea_extensions.dsc.expansion.expansion_specifier.value == 0);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.dsc.expansion.payload, "45894494") == 0);
+    REQUIRE(app.nmea0183().state().dsc.expansion.total_messages.value == 1);
+    REQUIRE(app.nmea0183().state().dsc.expansion.message_number.value == 1);
+    REQUIRE(app.nmea0183().state().dsc.expansion.query_flag == 'A');
+    REQUIRE(std::strcmp(app.nmea0183().state().dsc.expansion.sender_mmsi, "3380400790") == 0);
+    REQUIRE(app.nmea0183().state().dsc.expansion.expansion_specifier.value == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().dsc.expansion.payload, "45894494") == 0);
 
     feed(app, "CDDSI,3380400790,20", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.dsc.initiate.sentence_id, "DSI") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().dsc.initiate.sentence_id, "DSI") == 0);
 
     feed(app, "CDDSR,3380400790,ACK", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.dsc.response.field[1], "ACK") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().dsc.response.field[1], "ACK") == 0);
 
     feed(app, "ERETL,PORT,ASTERN", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.engine_telegraph.field[0], "PORT") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().engine_telegraph.field[0], "PORT") == 0);
 
     feed(app, "ECEVE,EVT1,INFO", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.event_message.field[0], "EVT1") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().event_message.field[0], "EVT1") == 0);
 
     feed(app, "FDFIR,FIRE1,ALARM", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.fire_detection.field[1], "ALARM") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().fire_detection.field[1], "ALARM") == 0);
 
     feed(app, "GPWDC,12.3,N,22.7796,K,TO1,FROM1", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.waypoint_distance_great_circle.sentence_id, "WDC") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().waypoint_distance_great_circle.sentence_id, "WDC") == 0);
 
     feed(app, "GPWDR,12.4,N,22.9648,K,TO2,FROM2", now_us);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.waypoint_distance_rhumb.field[4], "FROM2") == 0);
+    REQUIRE(std::strcmp(app.nmea0183().state().waypoint_distance_rhumb.field[4], "FROM2") == 0);
 
     feed(app, "GPWCV,6.7,N,DEST1", now_us);
     NEAR(app.store().model().navigation.rmb.closing_velocity_kn.value, 6.7f, 0.001f);
@@ -192,9 +192,9 @@ int main() {
     REQUIRE(app.store().model().navigation.gps.local_zone_minutes.value == 30);
 
     feed(app, "GPZDL,000930,12.3,N,VP1", now_us);
-    NEAR(app.store().model().nmea_extensions.variable_point.time_to_point_s.value, 570.0f, 0.001f);
-    NEAR(app.store().model().nmea_extensions.variable_point.distance_to_point_nmi.value, 12.3f, 0.001f);
-    REQUIRE(std::strcmp(app.store().model().nmea_extensions.variable_point.point_id, "VP1") == 0);
+    NEAR(app.nmea0183().state().variable_point.time_to_point_s, 570.0f, 0.001f);
+    NEAR(app.nmea0183().state().variable_point.distance_to_point_nmi, 12.3f, 0.001f);
+    REQUIRE(std::strcmp(app.nmea0183().state().variable_point.point_id, "VP1") == 0);
 
     feed(app, "GPZFO,123520,000315,ORIG1", now_us);
     NEAR(app.store().model().navigation.waypoint.origin_utc_time_s.value, 45320.0f, 0.001f);
