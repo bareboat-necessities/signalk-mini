3f185b74e304e0b56e328882cd085c64780fa91d
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-02 00:04:08 -0400

    export nmea tokenizer

--- modules/nmea0183/src/nmea0183_connector.hpp
+++ modules/nmea0183/src/nmea0183_connector.hpp
@@ -2,6 +2,7 @@
 
 #include "nmea0183_connector/nmea0183_helpers.hpp"
 #include "nmea0183_connector/special_sentence_hooks.hpp"
+#include "nmea0183_connector/nmea_tokenizer.hpp"
 #include "nmea0183_connector/sentence_parser.hpp"
 #include "nmea0183_connector/nmea_rx_connector.hpp"
 #include "nmea0183_connector/nmea_tx_connector.hpp"
