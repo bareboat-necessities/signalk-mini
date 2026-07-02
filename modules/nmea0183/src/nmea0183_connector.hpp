61c79fb1922b6ddf5bc75977f3096b4e3af45f9f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 23:25:17 -0400

    export nmea special sentence hooks

--- modules/nmea0183/src/nmea0183_connector.hpp
+++ modules/nmea0183/src/nmea0183_connector.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "nmea0183_connector/nmea0183_helpers.hpp"
+#include "nmea0183_connector/special_sentence_hooks.hpp"
 #include "nmea0183_connector/sentence_parser.hpp"
 #include "nmea0183_connector/nmea_rx_connector.hpp"
 #include "nmea0183_connector/nmea_tx_connector.hpp"
