63255e5a89c07de5444c5d6b2d372355d371d4db
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 23:25:07 -0400

    add nmea special sentence hook declarations

---
+++ modules/nmea0183/src/nmea0183_connector/special_sentence_hooks.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <stdint.h>
+#include "nmea0183_helpers.hpp"
+
+namespace nmea0183_connector {
+
+struct NmeaSentence;
+
+enum class NmeaSentenceFamily : uint8_t {
+    Standard = 0,
+    Ais,
+    SeaTalk,
+    Dsc,
+    Inmarsat,
+    Proprietary,
+    UnknownEncapsulation,
+};
+
+inline const char* nmea_sentence_family_name(NmeaSentenceFamily family) {
+    switch (family) {
+    case NmeaSentenceFamily::Standard: return "standard";
+    case NmeaSentenceFamily::Ais: return "ais";
+    case NmeaSentenceFamily::SeaTalk: return "seatalk";
+    case NmeaSentenceFamily::Dsc: return "dsc";
+    case NmeaSentenceFamily::Inmarsat: return "inmarsat";
+    case NmeaSentenceFamily::Proprietary: return "proprietary";
+    case NmeaSentenceFamily::UnknownEncapsulation: return "unknown_encapsulation";
+    }
+    return "unknown";
+}
+
+struct NmeaParserHooks {
+    void* user = nullptr;
+    void (*on_ais_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
+    void (*on_seatalk_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
+    void (*on_dsc_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
+    void (*on_inmarsat_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
+    void (*on_proprietary_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
+};
+
+inline bool nmea_span_starts_with(NmeaSpan span, const char* prefix) {
+    if (!prefix) return false;
+    for (size_t i = 0; prefix[i]; ++i) {
+        if (i >= span.length || !span.data || span.data[i] != prefix[i]) return false;
+    }
+    return true;
+}
+
+} // namespace nmea0183_connector
