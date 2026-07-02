8a3b9b6d713b4eed809a85fffc6f2160719d07c6
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-01 23:25:46 -0400

    classify nmea parser special sentence families

--- modules/nmea0183/src/nmea0183_connector/sentence_parser.hpp
+++ modules/nmea0183/src/nmea0183_connector/sentence_parser.hpp
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #include "nmea0183_helpers.hpp"
+#include "special_sentence_hooks.hpp"
 
 namespace nmea0183_connector {
 
@@ -21,6 +22,7 @@ struct NmeaSentence {
     uint8_t field_count;
     bool valid_checksum;
     char start_char;
+    NmeaSentenceFamily family;
 
     void clear() {
         raw[0] = '\0';
@@ -31,6 +33,7 @@ struct NmeaSentence {
         field_count = 0;
         valid_checksum = false;
         start_char = '$';
+        family = NmeaSentenceFamily::Standard;
         for (uint8_t i = 0; i < NMEA_MAX_FIELDS; ++i) fields[i] = NmeaSpan();
     }
 
@@ -47,6 +50,46 @@ inline bool talker_is(const NmeaSentence& s, const char* talker) {
     return nmea_span_equals(s.talker, talker);
 }
 
+inline bool sentence_is_any(const NmeaSentence& s, const char* a, const char* b) {
+    return sentence_is(s, a) || sentence_is(s, b);
+}
+
+inline bool sentence_is_any(const NmeaSentence& s, const char* a, const char* b, const char* c) {
+    return sentence_is(s, a) || sentence_is(s, b) || sentence_is(s, c);
+}
+
+inline bool nmea_is_ais_sentence(const NmeaSentence& s) {
+    return s.start_char == '!' && sentence_is_any(s, "VDM", "VDO", "VSD");
+}
+
+inline bool nmea_is_seatalk_sentence(const NmeaSentence& s) {
+    return nmea_span_starts_with(s.body, "STALK") || nmea_span_starts_with(s.body, "PSTALK") ||
+           (talker_is(s, "ST") && sentence_is(s, "ALK"));
+}
+
+inline bool nmea_is_dsc_sentence(const NmeaSentence& s) {
+    return sentence_is_any(s, "DSC", "DSE", "DSI");
+}
+
+inline bool nmea_is_inmarsat_sentence(const NmeaSentence& s) {
+    return nmea_span_starts_with(s.body, "PINM") || nmea_span_starts_with(s.body, "INM") ||
+           sentence_is_any(s, "IMK", "IMN", "IMR") || talker_is(s, "IC");
+}
+
+inline bool nmea_is_proprietary_sentence(const NmeaSentence& s) {
+    return s.body.length > 0 && s.body[0] == 'P';
+}
+
+inline NmeaSentenceFamily classify_nmea_sentence(const NmeaSentence& s) {
+    if (nmea_is_ais_sentence(s)) return NmeaSentenceFamily::Ais;
+    if (nmea_is_seatalk_sentence(s)) return NmeaSentenceFamily::SeaTalk;
+    if (nmea_is_dsc_sentence(s)) return NmeaSentenceFamily::Dsc;
+    if (nmea_is_inmarsat_sentence(s)) return NmeaSentenceFamily::Inmarsat;
+    if (nmea_is_proprietary_sentence(s)) return NmeaSentenceFamily::Proprietary;
+    if (s.start_char == '!') return NmeaSentenceFamily::UnknownEncapsulation;
+    return NmeaSentenceFamily::Standard;
+}
+
 class Nmea0183StreamParser {
 public:
     Nmea0183StreamParser() { reset(); }
@@ -59,6 +102,8 @@ public:
     }
 
     const char* last_error() const { return last_error_; }
+    void set_hooks(const NmeaParserHooks& hooks) { hooks_ = hooks; }
+    void clear_hooks() { hooks_ = NmeaParserHooks{}; }
 
     bool push(char c, NmeaSentence& out) {
         if (c == '$' || c == '!') {
@@ -126,10 +171,7 @@ public:
         const char* field_begin = out.body.data;
         const char* body_end = out.body.data + out.body.length;
         const char* comma = static_cast<const char*>(memchr(field_begin, ',', static_cast<size_t>(body_end - field_begin)));
-        if (!comma) {
-            last_error_ = "";
-            return true;
-        }
+        if (!comma) return finish_success(out);
         field_begin = comma + 1;
         while (out.field_count < NMEA_MAX_FIELDS && field_begin <= body_end) {
             const char* next = static_cast<const char*>(memchr(field_begin, ',', static_cast<size_t>(body_end - field_begin)));
@@ -138,15 +180,44 @@ public:
             if (!next) break;
             field_begin = next + 1;
         }
+        return finish_success(out);
+    }
+
+private:
+    bool finish_success(NmeaSentence& out) {
+        out.family = classify_nmea_sentence(out);
+        dispatch_hook(out);
         last_error_ = "";
         return true;
     }
 
-private:
+    void dispatch_hook(const NmeaSentence& out) const {
+        switch (out.family) {
+        case NmeaSentenceFamily::Ais:
+            if (hooks_.on_ais_sentence) hooks_.on_ais_sentence(out, hooks_.user);
+            break;
+        case NmeaSentenceFamily::SeaTalk:
+            if (hooks_.on_seatalk_sentence) hooks_.on_seatalk_sentence(out, hooks_.user);
+            break;
+        case NmeaSentenceFamily::Dsc:
+            if (hooks_.on_dsc_sentence) hooks_.on_dsc_sentence(out, hooks_.user);
+            break;
+        case NmeaSentenceFamily::Inmarsat:
+            if (hooks_.on_inmarsat_sentence) hooks_.on_inmarsat_sentence(out, hooks_.user);
+            break;
+        case NmeaSentenceFamily::Proprietary:
+            if (hooks_.on_proprietary_sentence) hooks_.on_proprietary_sentence(out, hooks_.user);
+            break;
+        default:
+            break;
+        }
+    }
+
     char buffer_[NMEA_MAX_SENTENCE_LEN];
     size_t pos_;
     bool collecting_;
     const char* last_error_;
+    NmeaParserHooks hooks_{};
 };
 
 } // namespace nmea0183_connector
