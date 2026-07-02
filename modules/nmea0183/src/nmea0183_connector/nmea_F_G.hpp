6fc6ccf28fcfd5c2daab3b00f42732d8c05ef9c2
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-02 17:14:33 -0400

    stop retaining FIR raw message record

--- modules/nmea0183/src/nmea0183_connector/nmea_F_G.hpp
+++ modules/nmea0183/src/nmea0183_connector/nmea_F_G.hpp
@@ -4,8 +4,8 @@
 
 template<typename Model>
 bool apply_fir(const NmeaSentence& sentence, Model& model, uint64_t now_us, ship_data_model::SensorSource source) {
-    (void)model;
-    return apply_raw_sentence_record(sentence, state_.fire_detection, "FIR", now_us, source);
+    (void)model; (void)now_us; (void)source;
+    return accept_unmodeled_sentence(sentence);
 }
 
 template<typename Model>
