57c1a324e511b500b06cd6212538c1239ffb4f3e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-02 00:39:40 -0400

    add navigation model fields for Smart0183 sentences

--- modules/data-model/src/ship_data_model/nav_data.hpp
+++ modules/data-model/src/ship_data_model/nav_data.hpp
@@ -30,12 +30,63 @@ struct GpsData {
 };
 
 template<typename Real = float>
+struct GpsAlmanacData {
+    Setting<SensorSource> source;
+    Stamped<int32_t> total_messages;
+    Stamped<int32_t> message_number;
+    Stamped<int32_t> satellite_prn;
+    Stamped<int32_t> gps_week;
+    Stamped<int32_t> sv_health;
+    Stamped<Real> eccentricity;
+    Stamped<Real> reference_time_s;
+    Stamped<Real> inclination_rad;
+    Stamped<Real> right_ascension_rate_rad_s;
+    Stamped<Real> sqrt_semi_major_axis;
+    Stamped<Real> argument_of_perigee_rad;
+    Stamped<Real> longitude_ascension_node_rad;
+    Stamped<Real> mean_anomaly_rad;
+    Stamped<Real> clock_f0_s;
+    Stamped<Real> clock_f1_s_s;
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
+struct WaypointArrivalData {
+    Setting<SensorSource> source;
+    Setting<bool> arrival_circle_entered;
+    Setting<bool> perpendicular_passed;
+    Stamped<Real> arrival_radius_nmi;
+    char waypoint_id[16] = {0};
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
+struct WaypointNavigationData {
+    Setting<SensorSource> source;
+    Stamped<Real> utc_time_s;
+    Stamped<Real> latitude_deg;
+    Stamped<Real> longitude_deg;
+    Stamped<Real> bearing_true_deg;
+    Stamped<Real> bearing_magnetic_deg;
+    Stamped<Real> distance_nmi;
+    char to_waypoint_id[16] = {0};
+    char from_waypoint_id[16] = {0};
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
 struct ApbData {
     Setting<SensorSource> source;
     Stamped<Real> track_deg;
     Stamped<Real> xte_nmi;
+    Stamped<Real> origin_to_destination_bearing_deg;
+    Stamped<Real> present_to_destination_bearing_deg;
+    Stamped<Real> heading_to_steer_deg;
     RangeSetting<Real> xte_gain_deg_per_nmi;
+    Setting<bool> arrival_circle_entered;
+    Setting<bool> perpendicular_passed;
     Setting<AutopilotMode> mode_hint;
+    char destination_id[16] = {0};
     char sender_id[3] = {0, 0, 0};
     uint64_t last_update_us = 0;
 };
@@ -58,6 +109,9 @@ struct RmbData {
 template<typename Real>
 struct NavigationData {
     GpsData<Real> gps;
+    GpsAlmanacData<Real> gps_almanac;
+    WaypointArrivalData<Real> waypoint_arrival;
+    WaypointNavigationData<Real> waypoint;
     ApbData<Real> apb;
     RmbData<Real> rmb;
 };
