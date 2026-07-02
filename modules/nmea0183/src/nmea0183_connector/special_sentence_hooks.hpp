a358f548c2570d602a607facd543bb81a9aa71c8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-02 11:35:35 -0400

    add query and NavTex hook families

--- modules/nmea0183/src/nmea0183_connector/special_sentence_hooks.hpp
+++ modules/nmea0183/src/nmea0183_connector/special_sentence_hooks.hpp
@@ -9,9 +9,11 @@ struct NmeaSentence;
 
 enum class NmeaSentenceFamily : uint8_t {
     Standard = 0,
+    Query,
     Ais,
     SeaTalk,
     Dsc,
+    NavTex,
     Inmarsat,
     Proprietary,
     UnknownEncapsulation,
@@ -20,9 +22,11 @@ enum class NmeaSentenceFamily : uint8_t {
 inline const char* nmea_sentence_family_name(NmeaSentenceFamily family) {
     switch (family) {
     case NmeaSentenceFamily::Standard: return "standard";
+    case NmeaSentenceFamily::Query: return "query";
     case NmeaSentenceFamily::Ais: return "ais";
     case NmeaSentenceFamily::SeaTalk: return "seatalk";
     case NmeaSentenceFamily::Dsc: return "dsc";
+    case NmeaSentenceFamily::NavTex: return "navtex";
     case NmeaSentenceFamily::Inmarsat: return "inmarsat";
     case NmeaSentenceFamily::Proprietary: return "proprietary";
     case NmeaSentenceFamily::UnknownEncapsulation: return "unknown_encapsulation";
@@ -32,9 +36,11 @@ inline const char* nmea_sentence_family_name(NmeaSentenceFamily family) {
 
 struct NmeaParserHooks {
     void* user = nullptr;
+    void (*on_query_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
     void (*on_ais_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
     void (*on_seatalk_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
     void (*on_dsc_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
+    void (*on_navtex_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
     void (*on_inmarsat_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
     void (*on_proprietary_sentence)(const NmeaSentence& sentence, void* user) = nullptr;
 };
