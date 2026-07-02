3f98bbe1153f8271b1391e76aca177e03a1edb94
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-02 19:18:19 -0400

    remove top-level deprecated model references

--- modules/data-model/src/ship_data_model/data_model.hpp
+++ modules/data-model/src/ship_data_model/data_model.hpp
@@ -80,44 +80,6 @@ struct DataModel {
     ShipImuData<Real> imu;
     ImuStateData<Real> imu_state;
     ImuCalibrationData<Real> imu_calibration;
-
-    // Compatibility references for older modules during the reorganization.
-    AutopilotServerData<Real>& server;
-    AutopilotStatusData<Real>& status;
-    AutopilotData<Real>& ap;
-    NavigationData<Real> navigation;
-    SeaData<Real>& water;
-    RudderData<Real>& rudder;
-    ServoData<Real>& servo;
-    ServoTelemetryData<Real>& servo_telemetry;
-    ServoCalibrationData<Real>& servo_calibration;
-    PilotsData<Real>& pilots;
-    PilotCommandData<Real>& pilot_command;
-    PilotOutputData<Real>& pilot_output;
-    TackData<Real>& tack;
-    AutopilotProfileData<Real>& profile;
-    ValuePublicationState<Real>& runtime_publication;
-
-    DataModel()
-        : env(sea),
-          server(autopilot.server),
-          status(autopilot.status),
-          ap(autopilot.controller),
-          navigation(nav, route, gnss, ais, propulsion, comm),
-          water(sea),
-          rudder(steering.rudder),
-          servo(steering.servo),
-          servo_telemetry(steering.servo_telemetry),
-          servo_calibration(steering.servo_calibration),
-          pilots(autopilot.pilots),
-          pilot_command(autopilot.pilot_command),
-          pilot_output(autopilot.pilot_output),
-          tack(autopilot.tack),
-          profile(autopilot.profile),
-          runtime_publication(autopilot.runtime_publication) {}
-
-    DataModel(const DataModel&) = delete;
-    DataModel& operator=(const DataModel&) = delete;
 };
 
 } // namespace ship_data_model
