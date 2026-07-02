37be7b48ce9beb826561d85c2c158ae33c1b19ba
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-02 13:36:29 -0400

    move MDA handler into H-N range

--- modules/nmea0183/src/nmea0183_connector/nmea_H_N.hpp
+++ modules/nmea0183/src/nmea0183_connector/nmea_H_N.hpp
@@ -124,6 +124,42 @@ bool apply_lwy(const NmeaSentence& sentence, Model& model, uint64_t now_us, ship
     return true;
 }
 
+template<typename Model>
+bool apply_mda(const NmeaSentence& sentence, Model& model, uint64_t now_us, ship_data_model::SensorSource source) {
+    if (sentence.field_count < 20) {
+        last_error_ = "short MDA";
+        return false;
+    }
+
+    float value = 0.0f;
+    if (sentence.field(1)[0] == 'I' && parse_real(sentence.field(0), value)) model.water.barometric_pressure_inhg.set(static_cast<Real>(value), now_us);
+    if (sentence.field(3)[0] == 'B' && parse_real(sentence.field(2), value)) model.water.barometric_pressure_bar.set(static_cast<Real>(value), now_us);
+    if (sentence.field(5)[0] == 'C' && parse_real(sentence.field(4), value)) model.water.air_temperature_c.set(static_cast<Real>(value), now_us);
+    if (sentence.field(7)[0] == 'C' && parse_real(sentence.field(6), value)) model.water.temperature_c.set(static_cast<Real>(value), now_us);
+    if (parse_real(sentence.field(8), value)) model.water.relative_humidity_percent.set(static_cast<Real>(value), now_us);
+    if (parse_real(sentence.field(9), value)) model.water.absolute_humidity_percent.set(static_cast<Real>(value), now_us);
+    if (sentence.field(11)[0] == 'C' && parse_real(sentence.field(10), value)) model.water.dew_point_c.set(static_cast<Real>(value), now_us);
+    if (sentence.field(13)[0] == 'T' && parse_real(sentence.field(12), value)) {
+        const Real direction = static_cast<Real>(wrap_360_deg(value));
+        model.water.wind_direction_deg.set(direction, now_us);
+        model.wind.truewind.direction_deg.set(direction, now_us);
+    }
+    if (sentence.field(15)[0] == 'M' && parse_real(sentence.field(14), value)) model.water.wind_direction_magnetic_deg.set(static_cast<Real>(wrap_360_deg(value)), now_us);
+    if (parse_knots(sentence.field(16), sentence.field(17), value)) {
+        model.water.wind_speed_kn.set(static_cast<Real>(value), now_us);
+        model.wind.truewind.speed_kn.set(static_cast<Real>(value), now_us);
+    }
+    if (sentence.field(19)[0] == 'M' && parse_real(sentence.field(18), value)) {
+        model.water.wind_speed_m_s.set(static_cast<Real>(value), now_us);
+        model.wind.truewind.speed_m_s.set(static_cast<Real>(value), now_us);
+    }
+    set_source(model.water.source, source);
+    set_source(model.wind.truewind.source, source);
+    model.water.last_update_us = now_us;
+    model.wind.truewind.last_update_us = now_us;
+    return true;
+}
+
 template<typename Wind>
 bool set_wind(Wind& wind, float angle_deg, float speed_kn, uint64_t now_us, ship_data_model::SensorSource source) {
     wind.direction_deg.set(static_cast<Real>(wrap_180_deg(angle_deg)), now_us);
