9b49091bf41a4ca630098630c389103714c57555
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-03 13:54:35 -0400

    Remove GNSS and AIS data from nav header

--- modules/data-model/src/ship_data_model/nav/nav_data.hpp
+++ modules/data-model/src/ship_data_model/nav/nav_data.hpp
@@ -5,135 +5,6 @@
 namespace ship_data_model {
 
 template<typename Real = float>
-struct GnssData {
-    Setting<SensorSource> source;
-    RangeSetting<Real> rate_hz;
-
-    Stamped<Real> timestamp_s;
-    Stamped<int32_t> date_day;
-    Stamped<int32_t> date_month;
-    Stamped<int32_t> date_year;
-    Stamped<int32_t> local_zone_hours;
-    Stamped<int32_t> local_zone_minutes;
-    Stamped<Real> track_deg;
-    Stamped<Real> speed_kn;
-
-    Stamped<Real> fix_lat_deg;
-    Stamped<Real> fix_lon_deg;
-    Stamped<Real> fix_alt_m;
-    Stamped<int32_t> fix_quality;
-    char fix_mode_indicator[8] = {0};
-    char navigational_status = 0;
-    Stamped<int32_t> satellites_used;
-    Stamped<Real> hdop;
-    Stamped<Real> geoidal_separation_m;
-    Stamped<Real> dgps_age_s;
-    Stamped<int32_t> dgps_station_id;
-    bool has_fix_json = false;
-
-    Stamped<Real> leeway_ground_deg;
-    Stamped<Real> compass_error_deg;
-    Stamped<Real> declination_deg;
-    Setting<int32_t> alignment_counter;
-
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct GnssAlmanacData {
-    Setting<SensorSource> source;
-    Stamped<int32_t> total_messages;
-    Stamped<int32_t> message_number;
-    Stamped<int32_t> satellite_prn;
-    Stamped<int32_t> gnss_week;
-    Stamped<int32_t> sv_health;
-    Stamped<Real> eccentricity;
-    Stamped<Real> reference_time_s;
-    Stamped<Real> inclination_rad;
-    Stamped<Real> right_ascension_rate_rad_s;
-    Stamped<Real> sqrt_semi_major_axis;
-    Stamped<Real> argument_of_perigee_rad;
-    Stamped<Real> longitude_ascension_node_rad;
-    Stamped<Real> mean_anomaly_rad;
-    Stamped<Real> clock_f0_s;
-    Stamped<Real> clock_f1_s_s;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct GnssFaultDetectionData {
-    Setting<SensorSource> source;
-    Stamped<Real> utc_time_s;
-    Stamped<Real> expected_error_lat_m;
-    Stamped<Real> expected_error_lon_m;
-    Stamped<Real> expected_error_alt_m;
-    Stamped<int32_t> failed_satellite_prn;
-    Stamped<Real> missed_detection_probability;
-    Stamped<Real> failed_satellite_bias_m;
-    Stamped<Real> failed_satellite_bias_stddev_m;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct GnssRangeResidualData {
-    Setting<SensorSource> source;
-    Stamped<Real> utc_time_s;
-    Stamped<int32_t> mode;
-    Stamped<Real> satellite_residual_m[12];
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct GnssNoiseStatisticsData {
-    Setting<SensorSource> source;
-    Stamped<Real> utc_time_s;
-    Stamped<Real> rms_range_stddev_m;
-    Stamped<Real> semi_major_stddev_m;
-    Stamped<Real> semi_minor_stddev_m;
-    Stamped<Real> semi_major_orientation_deg;
-    Stamped<Real> latitude_error_stddev_m;
-    Stamped<Real> longitude_error_stddev_m;
-    Stamped<Real> altitude_error_stddev_m;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct GnssDopActiveSatellitesData {
-    Setting<SensorSource> source;
-    char selection_mode = 0;
-    Stamped<int32_t> fix_mode;
-    Stamped<int32_t> satellite_prn[12];
-    Stamped<Real> pdop;
-    Stamped<Real> hdop;
-    Stamped<Real> vdop;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct GnssSatellitesInViewData {
-    Setting<SensorSource> source;
-    Stamped<int32_t> total_messages;
-    Stamped<int32_t> message_number;
-    Stamped<int32_t> satellites_in_view;
-    Stamped<int32_t> satellite_prn[4];
-    Stamped<Real> elevation_deg[4];
-    Stamped<Real> azimuth_true_deg[4];
-    Stamped<Real> snr_db[4];
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct GnssModelData {
-    GnssData<Real> fix;
-    GnssAlmanacData<Real> almanac;
-    GnssFaultDetectionData<Real> fault_detection;
-    GnssRangeResidualData<Real> range_residual;
-    GnssNoiseStatisticsData<Real> noise_statistics;
-    GnssDopActiveSatellitesData<Real> dop_active_satellites;
-    GnssSatellitesInViewData<Real> satellites_in_view;
-};
-
-template<typename Real = float>
 struct DatumReferenceData {
     Setting<SensorSource> source;
     char local_datum_code[8] = {0};
@@ -373,54 +244,6 @@ struct MultipleDataIdData {
 };
 
 template<typename Real = float>
-struct TrackedTargetData {
-    Setting<SensorSource> source;
-    Stamped<int32_t> target_number;
-    Stamped<Real> latitude_deg;
-    Stamped<Real> longitude_deg;
-    Stamped<Real> utc_time_s;
-    Stamped<Real> distance_nmi;
-    Stamped<Real> bearing_deg;
-    char bearing_reference = 0;
-    Stamped<Real> speed_kn;
-    Stamped<Real> course_deg;
-    char course_reference = 0;
-    Stamped<Real> cpa_distance_nmi;
-    Stamped<Real> tcpa_min;
-    char distance_units = 0;
-    char target_name[24] = {0};
-    char target_status = 0;
-    char reference_target = 0;
-    Stamped<int32_t> label_target_number[8];
-    char label[8][24] = {{0}};
-    Stamped<int32_t> label_count;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct AisDataLinkStatusData {
-    Setting<SensorSource> source;
-    char station_id[24] = {0};
-    char slot_status[16] = {0};
-    char status_text[48] = {0};
-    Stamped<int32_t> field_count;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct AisAddressedSafetyData {
-    Setting<SensorSource> source;
-    Stamped<int32_t> total_messages;
-    Stamped<int32_t> message_number;
-    char sequential_message_id[16] = {0};
-    char destination_mmsi[16] = {0};
-    char retransmit_flag = 0;
-    char safety_text[72] = {0};
-    Stamped<int32_t> field_count;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
 struct RmaData {
     Setting<SensorSource> source;
     char status = 0;
@@ -447,13 +270,6 @@ struct NavData {
     ScanningFrequencyData<Real> scanning_frequency;
     MultipleDataIdData<Real> multiple_data_id;
     RmaData<Real> rma;
-};
-
-template<typename Real = float>
-struct AisData {
-    TrackedTargetData<Real> tracked_target;
-    AisDataLinkStatusData<Real> data_link_status;
-    AisAddressedSafetyData<Real> addressed_safety;
 };
 
 template<typename Real = float>
