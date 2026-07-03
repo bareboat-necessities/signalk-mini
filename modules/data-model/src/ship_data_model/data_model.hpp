78f0ef72559b7ae86fa3c4a327a097540d553b86
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-03 13:54:44 -0400

    Split data model aggregate across domain directories

--- modules/data-model/src/ship_data_model/data_model.hpp
+++ modules/data-model/src/ship_data_model/data_model.hpp
@@ -1,41 +1,17 @@
 #pragma once
 
-#include "autopilot/autopilot_data.hpp"
-#include "ins/ship_imu_data.hpp"
+#include "autopilot/autopilot_model_data.hpp"
+#include "ins/ins_data.hpp"
 #include "nav/nav_data.hpp"
 #include "route/route_data.hpp"
+#include "gnss/gnss_data.hpp"
+#include "ais/ais_data.hpp"
 #include "wind_sea/wind_sea_data.hpp"
-#include "steering/steering_data.hpp"
+#include "env/env_data.hpp"
+#include "steering/steering_model_data.hpp"
+#include "notifications/notifications_data.hpp"
 
 namespace ship_data_model {
-
-template<typename Real = float>
-struct SteeringModelData {
-    RudderData<Real> rudder;
-    ServoData<Real> servo;
-    ServoTelemetryData<Real> servo_telemetry;
-    ServoCalibrationData<Real> servo_calibration;
-};
-
-template<typename Real = float>
-struct AutopilotModelData {
-    AutopilotServerData<Real> server;
-    AutopilotStatusData<Real> status;
-    AutopilotData<Real> controller;
-    PilotsData<Real> pilots;
-    PilotCommandData<Real> pilot_command;
-    PilotOutputData<Real> pilot_output;
-    TackData<Real> tack;
-    AutopilotProfileData<Real> profile;
-    ValuePublicationState<Real> runtime_publication;
-};
-
-template<typename Real = float>
-struct InsData {
-    ShipImuData<Real> imu;
-    ImuStateData<Real> imu_state;
-    ImuCalibrationData<Real> imu_calibration;
-};
 
 template<typename Real = float>
 struct FluidsData {
@@ -47,91 +23,6 @@ struct BatteriesData {
 
 template<typename Real = float>
 struct ElectricalData {
-};
-
-template<typename Real = float>
-struct AlertAcknowledgementData {
-    Setting<SensorSource> source;
-    char alarm_identifier[24] = {0};
-    Stamped<int32_t> local_alarm_number;
-    Stamped<int32_t> field_count;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct AlertAcknowledgementDetailData {
-    Setting<SensorSource> source;
-    char alarm_identifier[24] = {0};
-    Stamped<int32_t> local_alarm_number;
-    Stamped<int32_t> alert_instance;
-    char acknowledgement_state = 0;
-    char operator_id[24] = {0};
-    char detail[72] = {0};
-    Stamped<int32_t> field_count;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct AlertConditionData {
-    Setting<SensorSource> source;
-    char alarm_identifier[24] = {0};
-    Stamped<int32_t> local_alarm_number;
-    Stamped<int32_t> alert_instance;
-    char condition_state = 0;
-    char priority = 0;
-    char description[72] = {0};
-    Stamped<int32_t> field_count;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct NotificationAlertsData {
-    AlertAcknowledgementData<Real> acknowledgement;
-    AlertAcknowledgementDetailData<Real> acknowledgement_detail;
-    AlertConditionData<Real> condition;
-    NmeaTextRecordData<Real> fire;
-};
-
-template<typename Real = float>
-struct EventLogData {
-    Setting<SensorSource> source;
-    char event_id[24] = {0};
-    char event_type[16] = {0};
-    char event_state = 0;
-    char event_text[72] = {0};
-    Stamped<int32_t> field_count;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct EventData {
-    Setting<SensorSource> source;
-    char event_id[24] = {0};
-    char event_source[24] = {0};
-    char event_state = 0;
-    char event_text[72] = {0};
-    Stamped<int32_t> field_count;
-    uint64_t last_update_us = 0;
-};
-
-template<typename Real = float>
-struct NotificationMessagesData {
-    NmeaTextRecordData<Real> text;
-    EventLogData<Real> event_log;
-    EventData<Real> event;
-};
-
-template<typename Real = float>
-struct NotificationDscData {
-    NmeaTextRecordData<Real> interrogation;
-    NmeaTextRecordData<Real> response;
-};
-
-template<typename Real = float>
-struct NotificationsData {
-    NotificationAlertsData<Real> alerts;
-    NotificationMessagesData<Real> messages;
-    NotificationDscData<Real> dsc;
 };
 
 template<typename Real = float>
