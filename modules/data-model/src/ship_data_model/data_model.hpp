44edf109842b8bbc70fcede71632893e9e39d509
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-02 21:33:02 -0400

    Move modeled message records into notifications domain

--- modules/data-model/src/ship_data_model/data_model.hpp
+++ modules/data-model/src/ship_data_model/data_model.hpp
@@ -5,7 +5,6 @@
 #include "nav_data.hpp"
 #include "wind_sea_data.hpp"
 #include "steering_data.hpp"
-#include "nmea_sentence_data.hpp"
 
 namespace ship_data_model {
 
@@ -43,11 +42,29 @@ struct ElectricalData {
 };
 
 template<typename Real = float>
+struct NotificationTextData {
+    Setting<SensorSource> source;
+    char id[24] = {0};
+    char code[16] = {0};
+    char value[16] = {0};
+    char text[72] = {0};
+    Stamped<int32_t> field_count;
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
 struct AlarmNotificationsData {
+    NotificationTextData<Real> acknowledgement;
+    NotificationTextData<Real> alarm;
+    NotificationTextData<Real> equipment_status;
+    NotificationTextData<Real> event_log;
+    NotificationTextData<Real> event;
+    NotificationTextData<Real> fire;
 };
 
 template<typename Real = float>
 struct MessageNotificationsData {
+    NotificationTextData<Real> text;
 };
 
 template<typename Real = float>
@@ -76,7 +93,6 @@ struct DataModel {
     ElectricalData<Real> electrical;
     CommData<Real> comm;
     NotificationsData<Real> notifications;
-    NmeaModeledSentenceData<Real> nmea;
 
     // IMU remains in top-level storage until the inertial/navigation split is modeled separately.
     ShipImuData<Real> imu;
