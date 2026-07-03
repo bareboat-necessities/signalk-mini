f77a47efa293f6ee3a36be1b617897595d2526aa
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-03 13:53:03 -0400

    Split notification data into notifications directory

---
+++ modules/data-model/src/ship_data_model/notifications/notifications_data.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "../core_data_types.hpp"
+
+namespace ship_data_model {
+
+template<typename Real = float>
+struct AlertAcknowledgementData {
+    Setting<SensorSource> source;
+    char alarm_identifier[24] = {0};
+    Stamped<int32_t> local_alarm_number;
+    Stamped<int32_t> field_count;
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
+struct AlertAcknowledgementDetailData {
+    Setting<SensorSource> source;
+    char alarm_identifier[24] = {0};
+    Stamped<int32_t> local_alarm_number;
+    Stamped<int32_t> alert_instance;
+    char acknowledgement_state = 0;
+    char operator_id[24] = {0};
+    char detail[72] = {0};
+    Stamped<int32_t> field_count;
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
+struct AlertConditionData {
+    Setting<SensorSource> source;
+    char alarm_identifier[24] = {0};
+    Stamped<int32_t> local_alarm_number;
+    Stamped<int32_t> alert_instance;
+    char condition_state = 0;
+    char priority = 0;
+    char description[72] = {0};
+    Stamped<int32_t> field_count;
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
+struct NotificationAlertsData {
+    AlertAcknowledgementData<Real> acknowledgement;
+    AlertAcknowledgementDetailData<Real> acknowledgement_detail;
+    AlertConditionData<Real> condition;
+    NmeaTextRecordData<Real> fire;
+};
+
+template<typename Real = float>
+struct EventLogData {
+    Setting<SensorSource> source;
+    char event_id[24] = {0};
+    char event_type[16] = {0};
+    char event_state = 0;
+    char event_text[72] = {0};
+    Stamped<int32_t> field_count;
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
+struct EventData {
+    Setting<SensorSource> source;
+    char event_id[24] = {0};
+    char event_source[24] = {0};
+    char event_state = 0;
+    char event_text[72] = {0};
+    Stamped<int32_t> field_count;
+    uint64_t last_update_us = 0;
+};
+
+template<typename Real = float>
+struct NotificationMessagesData {
+    NmeaTextRecordData<Real> text;
+    EventLogData<Real> event_log;
+    EventData<Real> event;
+};
+
+template<typename Real = float>
+struct NotificationDscData {
+    NmeaTextRecordData<Real> interrogation;
+    NmeaTextRecordData<Real> response;
+};
+
+template<typename Real = float>
+struct NotificationsData {
+    NotificationAlertsData<Real> alerts;
+    NotificationMessagesData<Real> messages;
+    NotificationDscData<Real> dsc;
+};
+
+} // namespace ship_data_model
