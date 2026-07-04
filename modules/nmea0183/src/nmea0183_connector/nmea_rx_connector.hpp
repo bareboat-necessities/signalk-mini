32214fe23dabe930b26876857b6fc0a9f1076118
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-04 10:35:34 -0400

    Use TRF handler from O-Z NMEA header

--- modules/nmea0183/src/nmea0183_connector/nmea_rx_connector.hpp
+++ modules/nmea0183/src/nmea0183_connector/nmea_rx_connector.hpp
@@ -124,7 +124,7 @@ public:
         NMEA_APPLY("TPC", apply_tpc);
         NMEA_APPLY("TPR", apply_tpr);
         NMEA_APPLY("TPT", apply_tpt);
-        NMEA_APPLY("TRF", apply_trf_full);
+        NMEA_APPLY("TRF", apply_trf);
         NMEA_APPLY("TTM", apply_ttm);
         NMEA_APPLY("TXT", apply_txt);
         NMEA_APPLY("VBW", apply_vbw);
@@ -258,7 +258,6 @@ private:
 #include "nmea_A_E.hpp"
 #include "nmea_F_G.hpp"
 #include "nmea_H_N.hpp"
-#include "nmea_trf_override.hpp"
 #include "nmea_O_Z.hpp"
 };
 
