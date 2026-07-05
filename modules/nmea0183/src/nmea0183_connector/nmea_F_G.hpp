ccb59dd70a4435169d00e975754f8166c9aa3d1b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-05 11:33:48 -0400

    Include GNSS handler header from F-G header

--- modules/nmea0183/src/nmea0183_connector/nmea_F_G.hpp
+++ modules/nmea0183/src/nmea0183_connector/nmea_F_G.hpp
@@ -2,6 +2,8 @@
 
 // Included inside Nmea0183RxConnector.
 
+#include "nmea_gnss.hpp"
+
 template<typename Model>
 bool apply_fir(const NmeaSentence& sentence, Model& model, uint64_t now_us, ship_data_model::SensorSource source) {
     return apply_nmea_text_record(sentence, model.notifications.alerts.fire, now_us, source);
