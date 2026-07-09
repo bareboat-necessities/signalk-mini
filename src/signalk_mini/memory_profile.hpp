e8f77049a16ab3d4f554546cded1e3861742c841
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-07-09 00:23:31 -0400

    Reduce MCU memory profile defaults

--- src/signalk_mini/memory_profile.hpp
+++ src/signalk_mini/memory_profile.hpp
@@ -1,17 +1,22 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdint.h>
 
 namespace signalk_mini {
 
 #if defined(ARDUINO)
-static constexpr size_t DefaultModelChangeQueueCapacity = 256;
-static constexpr size_t DefaultSignalKJsonBufferSize = 512;
-static constexpr size_t DefaultSignalKBatchValues = 16;
+static constexpr size_t DefaultModelChangeQueueCapacity = 128;
+static constexpr size_t DefaultSignalKJsonBufferSize = 384;
+static constexpr size_t DefaultSignalKBatchValues = 8;
+static constexpr uint16_t DefaultMaxChangesPerTick = 16;
+static constexpr uint16_t DefaultSignalKMaxConnections = 4;
 #else
 static constexpr size_t DefaultModelChangeQueueCapacity = 512;
 static constexpr size_t DefaultSignalKJsonBufferSize = 1024;
 static constexpr size_t DefaultSignalKBatchValues = 32;
+static constexpr uint16_t DefaultMaxChangesPerTick = 32;
+static constexpr uint16_t DefaultSignalKMaxConnections = 8;
 #endif
 
 static constexpr size_t MaxSupportedModelChangeQueueCapacity = 65535;
